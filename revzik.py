b3d34e0aed4465d5f514716d65a088c67946cc7e
Vladimir Skovpan <vladimir.skovpan@gmail.com>
2020-08-29 18:52:14 +0200

    nu ti posral?

--- revzik.py
+++ revzik.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from urllib.request import urlretrieve
 from pprint import PrettyPrinter
 pp = PrettyPrinter()
@@ -6,12 +7,13 @@ pp = PrettyPrinter()
 api_key = 'OY62azZFNJ1rJgeMy7G9DbPPiFiSADveqDSM7am4'
 
 def fetchAsteroidNeowsFeed():
-  URL_NeoFeed = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY"
+  URL_NeoFeed = "https://api.nasa.gov/neo/rest/v1/feed?"
   params = {
       'api_key':api_key,
-      'start_date':'2020-01-22',
-      'end_date':'2020-01-23'
+      'start_date':'2020-08-22',
+      'end_date':'2020-08-29'
   }
   response = requests.get(URL_NeoFeed,params=params).json()
-  pp.pprint(response)
+  for dates in response["near_earth_objects"]:
+      pp.pprint(response["near_earth_objects"][dates][0]["name"])
 fetchAsteroidNeowsFeed()
